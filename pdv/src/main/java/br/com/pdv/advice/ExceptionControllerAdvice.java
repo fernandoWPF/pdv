1df567125bca40140c24a51357348ab317116e2d
fernando <fernando.fernandes@matera.com>
2019-06-22 16:21:59 -0300

    Criando Advice para tratamento de exceções e mensagens legíveis

--- pdv/src/main/java/br/com/pdv/advice/ExceptionControllerAdvice.java
+++ pdv/src/main/java/br/com/pdv/advice/ExceptionControllerAdvice.java
@@ -1,6 +1,8 @@
 package br.com.pdv.advice;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,5 +16,19 @@ public class ExceptionControllerAdvice {
     @ExceptionHandler(Exception.class)
     public ErrorDTO genericException(final Exception exception) {
         return new ErrorDTO(exception.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ErrorDTO validationException(final MethodArgumentNotValidException exception) {
+        StringBuilder builder = new StringBuilder();
+        BindingResult bindingResult = exception.getBindingResult();
+        builder.append("[Atributo]: ");
+        builder.append(bindingResult.getFieldError().getField());
+        builder.append(" [Conteudo]: ");
+        builder.append(bindingResult.getFieldValue(bindingResult.getFieldError().getField()));
+        builder.append(" [Erro]: ");
+        builder.append(bindingResult.getFieldError().getDefaultMessage());
+        return new ErrorDTO(String.valueOf(builder));
     }
 }
