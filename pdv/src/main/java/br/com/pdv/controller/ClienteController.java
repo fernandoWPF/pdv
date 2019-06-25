9b54f91afea094fd342623e343de888bd0177ea1
fernando <fernando.fernandes@matera.com>
2019-06-25 13:57:03 -0300

    Habilitando Pre Authorize do Spring Security

--- pdv/src/main/java/br/com/pdv/controller/ClienteController.java
+++ pdv/src/main/java/br/com/pdv/controller/ClienteController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,21 +27,25 @@ public class ClienteController {
     private ClienteService service;
 
     @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("hasAnyAuthority('INSERIR')")
     @PostMapping("/v1/clientes")
     public void salvar(@RequestBody @Valid ClienteRequestDTO request) {
         service.salvar(request);
     }
 
+    @PreAuthorize("hasAnyAuthority('ALTERAR')")
     @PutMapping("/v1/clientes/{id}")
     public void alterar(@PathVariable String id, @RequestBody @Valid ClienteRequestDTO request) {
         service.alterar(id, request);
     }
 
+    @PreAuthorize("hasAnyAuthority('CONSULTAR')")
     @GetMapping("/v1/clientes")
     public List<ClienteResponseDTO> buscarTodos() {
         return service.buscarTodos();
     }
     
+    @PreAuthorize("hasAnyAuthority('REMOVER')")
     @DeleteMapping("/v1/clientes/{id}")
     public void excluir(@PathVariable String id) {
         service.excluir(id);
