4f8bb334691e5a0a2f527a66b2516d97930c9bcb
fernando <fernando.fernandes@matera.com>
2019-06-25 13:57:29 -0300

    Adicionando tratamento para casos de erro no retorno da api

--- pdv/src/main/java/br/com/pdv/utils/CepUtils.java
+++ pdv/src/main/java/br/com/pdv/utils/CepUtils.java
@@ -2,6 +2,7 @@ package br.com.pdv.utils;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import br.com.pdv.configuracao.Propriedades;
@@ -16,13 +17,24 @@ public class CepUtils {
     private static final String CHAVE_URL_CEP = "cep.url";
 
     public CepResponseDTO buscaCepViaWebService(String cep) {
-        if(StringUtils.isBlank(cep)) {
+        if (StringUtils.isBlank(cep)) {
             throw new BusinessException("Cep nao pode ser vazio");
         }
         String url = montaUrl(cep);
         LOG.info("Fazendo requisicao para buscar CEP: {}", url);
+
         RestTemplate restTemplate = new RestTemplate();
-        return restTemplate.getForEntity(url, CepResponseDTO.class).getBody();
+        ResponseEntity<CepResponseDTO> response = restTemplate.getForEntity(url, CepResponseDTO.class);
+
+        if (response.getStatusCodeValue() != 200 || StringUtils.isNotBlank(response.getBody().getErro())) {
+            LOG.warn(
+                    "Requisicao retornou status diferente de 200 - OK, e/ou a requisicao retornou um objeto de erro. HttpStatus: {}. Body: {}",
+                    response.getStatusCodeValue(), response.getBody());
+            throw new BusinessException(
+                    "Requisicao retornou status diferente de 200 - OK, e/ou a requisicao retornou um objeto de erro.");
+        }
+
+        return response.getBody();
     }
 
     private String montaUrl(String cep) {
